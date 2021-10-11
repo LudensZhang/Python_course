4c4cdd66818783e4ceba60c4e3a572849efec149
HaohongZhang <iceqhdrop@outlook.com>
2021-10-11 11:57:36 +0000

    update README and basic-practice

--- basic_practice/basic-practice.py
+++ basic_practice/basic-practice.py
@@ -4,15 +4,37 @@ def factorial(a):
         a *= (i+1)
     return(a)
 
-def ways_OF_steps():
-    ways = 0
-    for step_IN_2 in range(51):
-        step_IN_1 = (100-step_IN_2*2)
-        ways += 
+def ways_OF_steps(n):
+    if n == 2: 
+        return 2
+    if n ==1:
+        return 1
+    return ways_OF_steps(n-2) + ways_OF_steps(n-1)
+
+def chance_OF_same_BIRTH(n):
+    q = 1 
+    for i in range(1, n):
+        q *= 1-i/365
+    print(f'the chance that at least {n} person have samebirthday is {1-q}')
+
+def find_edcba():
+    for i in range(10000, 30000):
+        ls_1 = list(x for x in str(i))
+        ls_2 = list(y for y in str(4*i))
+        ls_2.reverse()
+        if len(ls_2) > 5:
+            print("Can't find")
+            break
+        if ls_1 == ls_2:
+            print(f'find the edcba:{i}')
+            break
         
         
 
 
 if __name__ == '__main__':
     # print(factorial(5))
-    ways_OF_steps()
+    # print(f'the ways of 100 steps is {ways_OF_steps(50)}')
+    # chance_OF_same_BIRTH(100)
+    # find_edcba()
+    

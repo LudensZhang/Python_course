f5ac988af8a454d31791f2f7d69967e6a4f005f7
HaohongZhang <iceqhdrop@outlook.com>
2021-10-12 13:12:28 +0000

    update basic-practice

--- basic_practice/basic-practice.py
+++ basic_practice/basic-practice.py
@@ -1,8 +1,11 @@
+import numpy as np
+
+
 def factorial(a):
-    a = int(a)
+    b = int(a)
     for i in range(a-1):
-        a *= (i+1)
-    return(a)
+        b *= (i+1)
+    print(f'the factorial of {a} is {b}')
 
 def ways_OF_steps(n):
     if n == 2: 
@@ -40,13 +43,20 @@ def find_edcba():
         if ls_1 == ls_2:
             print(f'find the edcba:{i}')
             break
-        
-        
+
+def monte_pi(n):
+    random_POSIRION = np.random.uniform(-1, 1, size=[int(n), 2])
+    in_ROUND = 0
+    for i, ii in random_POSIRION:
+        if i**2+ii**2 <=1:
+            in_ROUND += 1
+    monte_PI = 4*in_ROUND/int(n)
+    print(f'the pi caclulated by monte-caro using {n} dots is {monte_PI}')
 
 
 if __name__ == '__main__':
-    # print(factorial(5))
+    # factorial(5)
     # print(f'the ways of 100 steps is {ways_OF_steps(50)}')
     # chance_OF_same_BIRTH(100)
     # find_edcba()
-    
+    # monte_pi(100)

7b775723ec0dbed43d17bff5bc109d75937f1605
HaohongZhang <iceqhdrop@outlook.com>
2021-10-15 02:12:19 +0000

    update basic_practice

--- basic_practice/basic-practice.py
+++ basic_practice/basic-practice.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 
 def factorial(a):
@@ -18,20 +19,8 @@ def chance_OF_same_BIRTH(n):
     q = 1 
     for i in range(1, n):
         q *= 1-i/365
-    print(f'the chance that at least {n} person have samebirthday is {1-q}')
+    print(f'the chance that at least 2 person  have samebirthday is {1-q}')
 
-def find_edcba():
-    for i in range(10000, 30000):
-        ls_1 = list(x for x in str(i))
-        ls_2 = list(y for y in str(4*i))
-        ls_2.reverse()
-        if len(ls_2) > 5:
-            print("Can't find")
-            break
-        if ls_1 == ls_2:
-            print(f'find the edcba:{i}')
-            break
-        
 def find_edcba():
     for i in range(10000, 30000):
         ls_1 = list(x for x in str(i))
@@ -51,10 +40,24 @@ def monte_pi(n):
         if i**2+ii**2 <=1:
             in_ROUND += 1
     monte_PI = 4*in_ROUND/int(n)
-    print(f'the pi caclulated by monte-caro using {n} dots is {monte_PI}')
+    print(f'the pi caclulated by monte-caro used {n} dots is {monte_PI}')
 
-def ages_OF_children():
-    
+def ages_of_children():
+    list_OF_oldest = []
+    list_OF_sub1 = []
+    list_OF_sub2 = []
+    for oldest in range(1, 37):
+        for sub1 in range(1, oldest):
+            for sub2 in range(1, oldest):
+                if oldest*sub1*sub2 == 36:
+                    list_OF_oldest.append(oldest)
+                    list_OF_sub1.append(sub1)
+                    list_OF_sub2.append(sub2)
+    df_AGE = pd.DataFrame(list(zip(list_OF_oldest,
+                                    list_OF_sub1,
+                                    list_OF_sub2)), columns=['oldest', 'sub1', 'sub2'])
+    df_AGE['age sum'] = df_AGE.apply(lambda x: x.sum(), axis=1)
+    print('df_AGE')
 
 if __name__ == '__main__':
     # factorial(5)
@@ -62,3 +65,4 @@ if __name__ == '__main__':
     # chance_OF_same_BIRTH(100)
     # find_edcba()
     # monte_pi(100)
+    ages_of_children()
--- main.py
+++ main.py
@@ -2,21 +2,14 @@ import pandas as pd
 import numpy as np 
 from plotnine import*
 
+def Multiplication_table():
+    for i in range(1, 10):
+        for ii in range(1, i+1):
+            print(f'{i}x{ii}={i*ii}', end='')
+            for iii in range(3 - len(str(i*ii))):
+                print(' ', end='')
+        print('\n')
 
-def find_edcba():
-    for i in range(10000, 30000):
-        ls_1 = list(x for x in str(i))
-        ls_2 = list(y for y in str(4*i))
-        ls_2.reverse()
-        if len(ls_2) > 5:
-            print("Can't find")
-            break
-        if ls_1 == ls_2:
-            print(f'find the edcba:{i}')
-            break
-        
-        
 
 if __name__ == '__main__':
-    find_edcba()
-    
+    Multiplication_table()

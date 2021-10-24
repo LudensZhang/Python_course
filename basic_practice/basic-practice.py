564c94ce67c273f4a172a5c4a0bddd97d01886da
iceQHdrop <iceqhdrop@outlook.com>
2021-10-24 14:29:02 +0800

    Update basic_practice

--- basic_practice/basic-practice.py
+++ basic_practice/basic-practice.py
@@ -8,6 +8,7 @@ def factorial(a):
         b *= (i+1)
     print(f'the factorial of {a} is {b}')
 
+
 def ways_OF_steps(n):
     if n == 2: 
         return 2
@@ -15,11 +16,13 @@ def ways_OF_steps(n):
         return 1
     return ways_OF_steps(n-2) + ways_OF_steps(n-1)
 
+
 def chance_OF_same_BIRTH(n):
     q = 1 
     for i in range(1, n):
         q *= 1-i/365
     print(f'the chance that at least 2 person  have samebirthday is {1-q}')
+    
 
 def find_edcba():
     for i in range(10000, 30000):
@@ -33,14 +36,16 @@ def find_edcba():
             print(f'find the edcba:{i}')
             break
 
+
 def monte_pi(n):
-    random_POSIRION = np.random.uniform(-1, 1, size=[int(n), 2])
+    random_POSIRION = np.random.uniform(-1, 1, size=[eval(n), 2])
     in_ROUND = 0
     for i, ii in random_POSIRION:
         if i**2+ii**2 <=1:
             in_ROUND += 1
-    monte_PI = 4*in_ROUND/int(n)
+    monte_PI = 4*in_ROUND/eval(n)
     print(f'the pi caclulated by monte-caro used {n} dots is {monte_PI}')
+    
 
 def ages_of_children():
     list_OF_oldest = []
@@ -56,13 +61,39 @@ def ages_of_children():
     df_AGE = pd.DataFrame(list(zip(list_OF_oldest,
                                     list_OF_sub1,
                                     list_OF_sub2)), columns=['oldest', 'sub1', 'sub2'])
-    df_AGE['age sum'] = df_AGE.apply(lambda x: x.sum(), axis=1)
-    print('df_AGE')
+    df_AGE['ages_sum'] = df_AGE.apply(lambda x: x.sum(), axis=1)
+    df_AGE.drop_duplicates(subset='ages_sum', keep=0, inplace=True)
+    for age_index in df_AGE.index:
+        print(f"if the sum of ages is {df_AGE.loc[age_index, 'ages_sum']},the ages of these children are {df_AGE.loc[age_index, df_AGE.columns[0:-1]].values.tolist()}")
 
+
+def minest_sum_gap():
+    a = np.random.randint(low=0, high=100, size=(5)).tolist()
+    b = np.random.randint(low=0, high=100, size=(5)).tolist()
+    print(f'a:{a}\nb:{b}')
+    list_COMBINED  = a+b
+    list_COMBINED.sort(reverse=True)
+    a_RESET = []
+    b_RESET = []
+    for i in range(len(list_COMBINED)):
+        if sum(a_RESET) > sum(b_RESET):
+            b_RESET.append(list_COMBINED[i])
+        else:
+            a_RESET.append(list_COMBINED[i])
+    print(f'Reseted lists are{a_RESET}{b_RESET}, the minest gap is {abs(sum(a_RESET)-sum(b_RESET))}')
+    
+    
+def hat_COLOR():
+    num_OF_red = 3
+    num_OF_white = 2
+    
+    
 if __name__ == '__main__':
     # factorial(5)
-    # print(f'the ways of 100 steps is {ways_OF_steps(50)}')
+    print(f'the ways of 100 steps is {ways_OF_steps(30)}')
     # chance_OF_same_BIRTH(100)
     # find_edcba()
     # monte_pi(100)
-    ages_of_children()
+    # ages_of_children()
+    # minest_sum_gap()
+    # hat_COLOR()

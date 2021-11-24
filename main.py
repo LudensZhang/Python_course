23272235322a750b49716914bdc71e854d3e313f
HaohongZhang <iceqhdrop@outlook.com>
2021-11-24 10:34:09 +0000

    update main.py

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 import pandas as pd
 import numpy as np 
 from plotnine import*
+from tkinter import*
 
-
-class BMI():
-    def __init__(self, height, weight):
-        self.height = height
-        self.weight = weight
-    
+class CalculateBMI:
+    def __init__(self):
+        window = Tk()
+        Label(window, text='name:').grid(row=1, column=3)
+        Entry(window).grid(row=1, column=4, padx=5, pady=5)
+        label(window, text='height:').grid(row=2, column=3)
+        self.height=StringVar()
+        Entry(window).grid(textvariable=self.height).grid(row=2, column=4, padx=5, pady=5)
+        label(window, text='weight:').grid(row=3, column=3)
+        self.weight=StringVar()
+        Entry(window, textvariable=self.weight).grid(row=3, column=4, padx=5, pady=5)
+        Button(window, text='calculate', command=self.calculate).grid(row=4, column=4, padx=5, pady=5)
+        window.mainloop()
+        
     def calculate(self):
-        result = self.weight/self.height**2
+        result = float(self.weight)/float(self.height)**2
         if result < 18.5:
             print(f'Your BMI is {result}, too slim')
         elif result < 25:
-            print(f'Your BMI is {result}, normal')
+            print()(f'Your BMI is {result}, normal')
         elif result < 30:
-            print(f'Your BMI is {result}, overweight')
+            print()(f'Your BMI is {result}, overweight')
         else:
             print(f'Your BMI is {result}, obesity')
 
 
-if __name__ == '__main__':
-    a = BMI(1.7, 60)
-    a.calculate()
+    
